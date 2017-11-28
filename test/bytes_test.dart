765c13eb9e102c693f5c50cd44a65f59266d43f9
kyorohiro <kyorohiro@gmail.com>
2017-11-29 04:39:15 +0900

    ..

--- test/bytes_test.dart
+++ test/bytes_test.dart
@@ -4,27 +4,30 @@ import 'dart:typed_data';
 Future<dynamic>main() async {
   ByteData data = new ByteData(1000);
   Uint8List uint8Buff = data.buffer.asUint8List();
-  for(int i=0;i<1000;i++) {
+  int s = 1000;
+  int l = 5*10000;
+  for(int i=0;i<s;i++) {
     data.buffer.asUint8List()[i] = i;
   }
 
   unit.test("test1", (){
     int start = new DateTime.now().millisecondsSinceEpoch;
     int v = 0;
-    for(int i=0;i<1000;i++) {
-      for (int i = 0; i < 1000; i++) {
-        v += data.buffer.asUint8List(i)[0];
+    for(int i=0;i<s;i++) {
+      for (int i = 0; i < l; i++) {
+        v += data.buffer.asUint8List(i%s)[0];
       }
     }
     int end = new DateTime.now().millisecondsSinceEpoch;
     print("#1 ${end - start} ${v}");
   });
+
   unit.test("test2", (){
     int start = new DateTime.now().millisecondsSinceEpoch;
     int v = 0;
-    for(int i=0;i<1000;i++) {
-      for (int i = 0; i < 1000; i++) {
-        v += data.buffer.asUint8List()[i];
+    for(int i=0;i<s;i++) {
+      for (int i = 0; i < l; i++) {
+        v += data.buffer.asUint8List()[i%s];
       }
     }
     int end = new DateTime.now().millisecondsSinceEpoch;
@@ -34,12 +37,24 @@ Future<dynamic>main() async {
   unit.test("test3", (){
     int start = new DateTime.now().millisecondsSinceEpoch;
     int v = 0;
-    for(int i=0;i<1000;i++) {
-      for (int i = 0; i < 1000; i++) {
-        v += uint8Buff[i];
+    for(int i=0;i<s;i++) {
+      for (int i = 0; i < l; i++) {
+        v += data.getUint8(i%s);
       }
     }
     int end = new DateTime.now().millisecondsSinceEpoch;
     print("#3 ${end - start} ${v}");
+  });
+
+  unit.test("test4", (){
+    int start = new DateTime.now().millisecondsSinceEpoch;
+    int v = 0;
+    for(int i=0;i<s;i++) {
+      for (int i = 0; i < l; i++) {
+        v += uint8Buff[i%s];
+      }
+    }
+    int end = new DateTime.now().millisecondsSinceEpoch;
+    print("#4 ${end - start} ${v}");
   });
 }
